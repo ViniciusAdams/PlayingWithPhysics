5c16ce5d8f205391fcb638e4381e9040b96730dd
vinia <viniusadams98@gmail.com>
2023-09-03 23:35:42 +0100

    creating new class to calculate how many objects would be necessary to cover the earth's circuference

---
+++ src/EarthPart1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class EarthPart1 {
+public static void main (String [] args){
+    Scanner scanner = new Scanner (System.in);
+final double AVARAGE_EARTH = 6371.0;
+
+System.out.print ("Enter the size of the object in meters");
+
+double earthCircuferenceMeters = 2;
+}
+
+}
